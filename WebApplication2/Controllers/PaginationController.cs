9ffec41e911c51aa649353bb0c3a6e9b10df1ca3
tpsumeta <tpsumeta@gmail.com>
2017-12-20 15:58:35 +0700

    Pagination

---
+++ WebApplication2/Controllers/PaginationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PaginationController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            string page = HttpContext.Request.Query["id"].ToString();
+
+            var pagModel = new PaginationViewModel();
+
+            pagModel.start = 1;
+            pagModel.stop = 5;
+
+            if (page != null && page != "")
+            {
+                pagModel.number = Int32.Parse(page);
+                pagModel.start = pagModel.number - 2;
+                pagModel.stop = pagModel.number + 2;
+                if (pagModel.start <=0)
+                {
+                    pagModel.start = 1;
+                    pagModel.stop = 5;
+                }
+            }
+
+
+            return View(pagModel);
+        }
+
+
+   
+
+    }
+}
---
+++ WebApplication2/Models/PaginationViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WebApplication2.Models
+{
+    public class PaginationViewModel
+    {
+        public int number { get; set; }
+        public string active { get; set; }
+        public int start { get; set; }
+        public int stop { get; set; }
+
+
+    }
+}
