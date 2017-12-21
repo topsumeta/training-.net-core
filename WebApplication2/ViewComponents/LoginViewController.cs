9487bdef56f524a27a0af138d2b6ddc53c7094a5
tpsumeta <tpsumeta@gmail.com>
2017-12-21 09:40:23 +0700

    view components

---
+++ WebApplication2/ViewComponents/LoginViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Controllers
+{
+    public class LoginViewController : ViewComponent
+    {
+        public IViewComponentResult Invoke(int maxPriorty,bool isDone)
+        {
+            int model = 1;
+
+            return View(model);
+        }
+
+
+     
+    }
+}
