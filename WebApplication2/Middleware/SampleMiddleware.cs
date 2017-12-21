a18a9e58d30d9c248134635d05ecfd7f35fd5cbc
tpsumeta <tpsumeta@gmail.com>
2017-12-21 14:32:46 +0700

    Middleware

---
+++ WebApplication2/Middleware/SampleMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Middleware
+{
+    public class SampleMiddleware
+    {
+
+        private readonly RequestDelegate next;
+
+        public SampleMiddleware()
+        {
+            this.next = next;
+
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            return next(context);
+        }
+
+    }
+}
--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -27,6 +27,8 @@ namespace WebApplication2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSession();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
